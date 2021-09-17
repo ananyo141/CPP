05052ae720b9497249d8872bfd02d71e3c275160
ananyo141 <ananyo141@gmail.com>
2021-09-17 16:03:11 +0530

    fix secondsToDays

--- C++ Primer Plus/Chapter 2/secondsToDays.cpp
+++ C++ Primer Plus/Chapter 2/secondsToDays.cpp
@@ -7,12 +7,20 @@ const char HOURS_TO_DAY (24);
 int main() {
     using namespace std;
 
-    long long seconds;
+    long long num_seconds;
     cout << "Enter the number of seconds: ";
-    cin >> seconds;
-    cout << seconds << " seconds = ";
+    cin >> num_seconds; 
+    cout << num_seconds << " seconds = ";
 
-    unsigned short days = seconds / (SECS_TO_MIN * MINS_TO_HOUR * HOURS_TO_DAY);
+    unsigned short days    = num_seconds / (SECS_TO_MIN * MINS_TO_HOUR * HOURS_TO_DAY);
+    num_seconds -= days * (SECS_TO_MIN * MINS_TO_HOUR * HOURS_TO_DAY);
+    unsigned short hours   = num_seconds / (SECS_TO_MIN * MINS_TO_HOUR);
+    num_seconds -= hours * (SECS_TO_MIN * MINS_TO_HOUR);
+    unsigned short mins    = num_seconds / (SECS_TO_MIN);
+    num_seconds -= mins * SECS_TO_MIN;
+
+    cout << days << " days, " << hours << " hours, "
+         << mins << " mins, " << num_seconds << " seconds.\n";
 
     return 0;
 }
